f2046d60104e697555b7f7a55a85ba68b4eb2dc8
Maria Cecylia Balbino <cecyliasb@gmail.com>
2022-01-09 20:15:28 -0300

    game over

---
+++ DesafioNasAlturas/Assets/Scripts/Diretor.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Diretor : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject imagemGameOver;
+    private Aviao aviao;
+
+    private void Start()
+    {
+        this.aviao = GameObject.FindObjectOfType<Aviao>();
+    }
+
+    public void FinalizarJogo()
+    {
+        Time.timeScale = 0;
+        
+        //habilitar a imagem de game over
+        this.imagemGameOver.SetActive(true);
+    }
+
+    public void ReiniciarJogo()
+    {
+        this.imagemGameOver.SetActive(false);
+        Time.timeScale = 1;
+        this.aviao.Reiniciar();
+        this.DestruirObstaculos();
+    }
+
+    private void DestruirObstaculos()
+    {
+        Obstaculo[] obstaculos = GameObject.FindObjectsOfType<Obstaculo>();
+        foreach (Obstaculo obstaculo in obstaculos)
+        {
+            obstaculo.Destruir();
+        }
+    }
+}
