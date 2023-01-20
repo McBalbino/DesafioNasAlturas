5681a400a3cfd53f1bb7183bd2154d0a67179930
Maria Cecylia Balbino <cecyliasb@gmail.com>
2023-01-19 22:41:01 -0300

    Criando animações

---
+++ DesafioNasAlturas/Assets/Scripts/MaoPiscando.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaoPiscando : MonoBehaviour
+{
+    private SpriteRenderer imagem;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        this.imagem = this.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            this.Desaparecer();
+        }
+    }
+
+    private void Desaparecer()
+    {
+        this.imagem.enabled = false;
+    }
+}
